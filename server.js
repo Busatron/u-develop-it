1457d96426fa4099bc1e4d1b69572317799d0ee5
Busatron <bussea38@gmail.com>
2022-08-21 17:26:04 -0500

    your instructions suck

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
+const mysql = require('mysql2');
 const express = require('express');
+
 const PORT = process.env.PORT || 3001
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Helodown40!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+  });
+
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
 
 app.get('/', (req, res) => {
     res.json({
